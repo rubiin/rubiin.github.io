65f668a8eb08baf5c54320040bb0a1e320a3106b
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-07-23 23:53:25 +0545

    fix: linkedin username

--- src/config/index.js
+++ src/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/xidharth',
+      url: 'https://www.linkedin.com/in/rubiin',
     },
     {
       name: 'Dev.to',
