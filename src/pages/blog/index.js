2e16b4189dddacfc85c4341fa351a954bffb91ed
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-08-03 00:58:41 +0545

    fix: add blogs and tags together

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -58,7 +58,7 @@ const StyledGrid = styled.div`
 
   .posts {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(1100px, 1fr));
     grid-gap: 10px;
     position: relative;
     ${media.desktop`grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));`};
