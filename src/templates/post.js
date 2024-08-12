32c9d2bf4162959f76dc0294112bb4bf5688dd07
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-08-12 13:41:14 +0545

    fix: add cover image css

--- src/templates/post.js
+++ src/templates/post.js
@@ -45,7 +45,8 @@ const StyledShareContainer = styled.div`
 
 const StyledImg = styled.img`
 object-fit: cover;
-
+border-radius: ${theme.borderRadius};
+box-shadow:0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19) !important;
 `;
 
 
